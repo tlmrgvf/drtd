9a0bbf93d0c581e858d87037966893e030a160fb
Till Mayer <till.mayer@web.de>
2020-08-02 20:32:45 +0200

    DownSampler: Low-pass filter samples before downsampling
    Just averaging the samples appears to not be enough

--- src/main/java/de/tlmrgvf/drtd/utils/DownSampler.java
+++ src/main/java/de/tlmrgvf/drtd/utils/DownSampler.java
@@ -1,17 +1,25 @@
 package de.tlmrgvf.drtd.utils;
 
+import de.tlmrgvf.drtd.dsp.component.biquad.BiquadFilter;
+import de.tlmrgvf.drtd.dsp.component.biquad.GenericBiquadFilter;
+
 public final class DownSampler {
     private final float targetSampleRatio;
     private float currentSampleRatio;
     private float average;
     private float averagedValues;
+    private final BiquadFilter lowpassFilter;
 
     public DownSampler(float sourceSampleRate, float targetSampleRate) {
         targetSampleRatio = sourceSampleRate / targetSampleRate;
         currentSampleRatio = 0;
+        lowpassFilter = new BiquadFilter(GenericBiquadFilter.Type.LOWPASS,
+                (int) sourceSampleRate,
+                targetSampleRate / 2);
     }
 
     public Float sample(float input) {
+        input = lowpassFilter.filterSample(input);
         Float ret = null;
 
         if (currentSampleRatio >= 0) {
